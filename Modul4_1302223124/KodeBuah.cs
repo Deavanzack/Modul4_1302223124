8373ecb19c4eeb342bae9dfb595c5cfb05f4ed68
Your Name <you@example.com>
2024-03-14 10:24:15 +0700

    menambahkan class KodeBuah

---
+++ Modul4_1302223124/KodeBuah.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_1302223124
+{
+    internal class KodeBuah
+    {
+        public enum buah
+        {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung,
+            Kurma,
+            Durian,
+            Anggur,
+            Melon,
+            Semangka,
+        }
+        public static String getcodebuah(buah code)
+        {
+            String[] kode = {"A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00" , "J00" , "K00" , "L00" , "Ooo" }
+            return kode[(int)code];
+        }
+    }
+}
